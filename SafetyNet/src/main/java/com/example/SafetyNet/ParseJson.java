077fdff801fd0f5a8913297a932b297b47ba947d
Nikola Pencic <46595153+Pencic@users.noreply.github.com>
2021-02-05 14:51:50 +0100

    Parsed Json

--- SafetyNet/src/main/java/com/example/SafetyNet/ParseJson.java
+++ SafetyNet/src/main/java/com/example/SafetyNet/ParseJson.java
@@ -1,5 +1,24 @@
 package com.example.SafetyNet;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ParseJson {
+	
+public static void Parse() {
+		
+		try {
+			 CompositeModel model = new ObjectMapper().readValue(new File("src/main/resources/data.json"), CompositeModel.class);
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
